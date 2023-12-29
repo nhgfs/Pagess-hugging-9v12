af60df71cec1111c27512855464290eae3c7d245
nhgfs <121149828+nhgfs@users.noreply.github.com>
2023-12-29 04:40:37 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '4hfjjf-8080.csb.app'
+      url.hostname = 'monoock-youoou.hf.space'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
